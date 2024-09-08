ac36c35b4b248870050db67ae94daab82a3aed3f
Ayush Singh <ayushsingh01042003@gmail.com>
2024-09-08 16:31:53 +0530

    Initialise Spring Boot Project

---
+++ src/main/java/com/Ayush/Cryptik/CryptikApplication.java
@@ -0,0 +1,13 @@
+package com.Ayush.Cryptik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ayush/Cryptik/CryptikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ayush.Cryptik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
